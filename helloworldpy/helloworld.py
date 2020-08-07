649f141e20c12d77f1e59b88b0052d7cfcc6e960
Roshan Fernandes <rfernandes@freewheel.tv>
2020-08-07 12:41:42 -0400

    use of environment variables

--- helloworldpy/helloworld.py
+++ helloworldpy/helloworld.py
@@ -1,5 +1,13 @@
 import datetime
+import os
+
 x = datetime.datetime.now()
 print "Hello World - Python!!!!"
 print "Current date/time: "
 print x
+
+if "USERNAME" in os.environ:
+    print os.getenv("USERNAME")
+else:
+    print "no user name"
+
